1c9ea02a5eae5d50357189082680bd40f9a038d4
robertSovar <133514926+robertSovar@users.noreply.github.com>
2024-11-17 14:39:41 +0200

    Update validateAuth function

--- routes/api/commentRoutes.js
+++ routes/api/commentRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import commentController from "../../controllers/commentController.js";
-import validateAuth from "../../middleware/validateAuth.js";
+import validateAuth from "../../controllers/userController.js";
 
 const router = express.Router();
 
