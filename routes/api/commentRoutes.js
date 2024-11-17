13e2c53462dac744a31bf3d9e496af7734c77705
robertSovar <133514926+robertSovar@users.noreply.github.com>
2024-11-17 14:56:16 +0200

    Fix bug for comment route

--- routes/api/commentRoutes.js
+++ routes/api/commentRoutes.js
@@ -1,12 +1,20 @@
 import express from "express";
 import commentController from "../../controllers/commentController.js";
-import validateAuth from "../../controllers/userController.js";
+import userController from "../../controllers/userController.js";
 
 const router = express.Router();
 
-router.get("/:articleId", validateAuth, commentController.getCommentsByArticle);
+router.get(
+  "/:articleId",
+  userController.validateAuth,
+  commentController.getCommentsByArticle
+);
 router.post("/", commentController.addComment);
-router.put("/:id", validateAuth, commentController.editComment);
-router.delete("/:id", validateAuth, commentController.deleteComment);
+router.put("/:id", userController.validateAuth, commentController.editComment);
+router.delete(
+  "/:id",
+  userController.validateAuth,
+  commentController.deleteComment
+);
 
 export default router;
