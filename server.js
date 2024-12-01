cf5b1a23365775717959179d3518187309f32e51
robertSovar <133514926+robertSovar@users.noreply.github.com>
2024-12-01 12:39:51 +0200

    Update

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 import app from "./app.js";
 import colors from "colors";
-if (process.env.NODE_ENV !== "Production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`.green));
-}
+
+export default app;
